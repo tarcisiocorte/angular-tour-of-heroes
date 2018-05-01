2df6d69a15627e6aa708fea38746ce811db1b721
tarcisiocorte <tarcisiocorte@gmail.com>
2018-04-30 22:56:50 -0300

    first one way databind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { GrayFooterComponent } from './gray-footer/gray-footer.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, GrayFooterComponent],
+  declarations: [AppComponent, HeroesComponent, GrayFooterComponent, HeroDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
